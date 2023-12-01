72a8af4afbd36a88c366095461eb0471cd013e19
ANAIS DINA <azddr@outlook.fr>
2023-12-01 11:21:36 +0100

    fin

--- page3sinscrire.js
+++ page3sinscrire.js
@@ -4,16 +4,18 @@ window.onload = function() {
     const title = document.querySelector('.title');
 
     // Cacher le contenu initial pendant 15 secondes
+    
     setTimeout(function() {
-        pageContent.style.display = 'none';
+        // pageContent.style.display = 'none';
 
         // Afficher le titre avec l'effet de distortion après 15 secondes
         loadingScreen.style.display = 'flex';
+        title.classList.add('distortion');
         setTimeout(function() {
-            loadingScreen.style.display = 'none';
-            pageContent.style.display = 'block';
-            title.classList.add('distortion');
+            // loadingScreen.style.display = 'none';
+            location.assign('Accueil/TEST-ACCUEIL.html')
         }, 3000); // Changez la durée d'attente (en millisecondes) selon vos besoins
-    }, 15000); // Changez la durée d'attente (en millisecondes) selon vos besoins
+    }, 10000); // Changez la durée d'attente (en millisecondes) selon vos besoins
+    
 };
 
